9ad3423388dc5a0632dc897f472a69b90d7c8ae6
Egor Matveev <egormsm@gmail.com>
2020-12-14 15:57:11 +0300

    Выполнил первую часть обязательного

--- script/script.js
+++ script/script.js
@@ -1,17 +1,50 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
+
 window.addEventListener(`DOMContentLoaded`, () => {
 
+    function getZero(num) {
+        if (num > 0 && num < 10) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
 
     // Timer
     function countTimer(deadline) {
-        const hours = document.querySelector(`#timer-hours`),
-            minutes = document.querySelector(`#timer-minutes`),
-            seconds = document.querySelector(`#timer-seconds`),
-            dateStop = new Date(deadline).getTime(),
-            dateNow = new Date().getTime();
+        let timerHours = document.querySelector(`#timer-hours`),
+            timerMinutes = document.querySelector(`#timer-minutes`),
+            timerSeconds = document.querySelector(`#timer-seconds`);
 
-        console.log(dateNow);
-        console.log(dateStop);
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hour = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hour, minutes, seconds };
+        }
+
+        function updateClock() {
+            let timer = getTimeRemaining();
+
+            timerHours.innerHTML = getZero(timer.hour);
+            timerMinutes.innerHTML = getZero(timer.minutes);
+            timerSeconds.innerHTML = getZero(timer.seconds);
+
+            if (timer.timeRemaining > 0) {
+                setInterval(updateClock, 1000);
+            }   else {
+                clearInterval(updateClock);
+                timerHours.innerHTML = '00';
+                timerMinutes.innerHTML = '00';
+                timerSeconds.innerHTML = '00';
+            }
+        }
+
+        updateClock();
     }
-    countTimer('01 january 2021');
+    countTimer('15 december 2020');
 });

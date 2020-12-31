aa3c4fd0f4128235c0a0151c2972c2b39dcabd0b
Egor Matveev <egormsm@gmail.com>
2020-12-31 14:38:46 +0300

    fixed menu" git push

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,17 +1,57 @@
+
+const animScroll = () => {
+
+    const target = event.target.closest('[href^="#"]'),
+        speed = 0.57;
+
+    if (target) {
+        const pageY = window.pageYOffset,
+            regEx = target.href.replace(/[^#]*(.*)/, '$1'),
+            distTopPosition = document.querySelector(regEx).getBoundingClientRect().top;
+
+        let start = 0;
+
+        const step = time => {
+            if (!start) start = time;
+
+            const progress = time - start;
+
+            const e = (distTopPosition < 0 ?
+                Math.max(pageY - progress / speed, pageY + distTopPosition) :
+                Math.min(pageY + progress / speed, pageY + distTopPosition));
+
+            window.scrollTo(0, e);
+
+            if (e < pageY + distTopPosition) requestAnimationFrame(step);
+        };
+
+        requestAnimationFrame(step);
+
+    }
+};
+
 const toggleMenu = () => {
 
-    const btnMenu = document.querySelector(`.menu`),
-        menu = document.querySelector(`menu`),
-        closeBtn = document.querySelector(`.close-btn`),
-        menuItems = menu.querySelectorAll(`ul>li`);
+    const menu = document.querySelector('menu'),
+        body = document.querySelector('body');
 
     const handlerMenu = () => {
-        menu.classList.toggle(`active-menu`);
+        menu.classList.toggle('active-menu');
+        animScroll();
     };
-    btnMenu.addEventListener(`click`, handlerMenu);
-    closeBtn.addEventListener(`click`, handlerMenu);
 
-    menuItems.forEach(elem => elem.addEventListener(`click`, handlerMenu));
+    body.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.closest('.menu')) {
+            handlerMenu();
+        } else if (!target.closest('.active-menu') && menu.classList.contains('active-menu')) {
+            handlerMenu();
+        } else if (target.closest('.close-btn')) {
+            handlerMenu();
+        } else if (target.closest('a')) {
+            handlerMenu();
+        }
+    });
 };
 
 export default toggleMenu;
